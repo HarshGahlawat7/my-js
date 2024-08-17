3ee668338fb989c2853eabe8f95c4045b049d610
HarshGahlawat7 <gahlawatharsh69@gmail.com>
2024-08-17 12:53:48 +0000

    container config done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treats all JS code as newer version of JS
+
+// alert(3+3) //This will not work as we are using nodejs , not a browser
+
+//Datatypes can be defined using let 
+
+// Example
+let name = "Harsh" //String datatype
+let age = 18 //Number datatype
+let isLoggedin = true //Boolean datatype
+let state; //Undefined datatype
+
+//Datatypes are ->
+
+//Number -> 2 to power 53
+//bigint 
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+console.log(typeof undefined); //Output will be undefined
+console.log(typeof null); //Output will be object
