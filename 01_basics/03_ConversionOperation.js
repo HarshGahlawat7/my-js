fa21a6a528a1c811bfdb6b817eba80fbbafb881a
HarshGahlawat7 <gahlawatharsh69@gmail.com>
2024-08-24 05:37:01 +0000

    new info added

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,15 +1,24 @@
-let score = "45" //This is a string 
+// let score = "45" //This is a string 
 
-console.log(typeof score); //This will give output string because the number 45 was betweeen commas , it it was not in commas , it would have given number 
+// console.log(typeof score); //This will give output string because the number 45 was betweeen commas , it it was not in commas , it would have given number 
 
-let valueInNumber = Number(score)
-console.log(typeof score); //This will give number datatype because score is converted into number
+// let valueInNumber = Number(score)
+// console.log(typeof score); //This will give number datatype because score is converted into number
 
-let marks = "67abc"
-console.log(typeof marks); //This will give NaN i.e. not a number datatype
+// let marks = "67abc"
+// console.log(typeof marks); //This will give NaN i.e. not a number datatype
 //But type of NaN is number
 
 //Conversion to Number
 // "33" => 33
 // "33har" or undefined => NaN
 // true => 1 , false => 0
+
+//*****************************Operations************************************//
+
+let str1 = "hello"
+let str2 = " harsh"
+
+let str3 = str1 + str2
+
+console.log(str3)
